dcd52e0f918ec4afe414207d2ff923483b9d5c6c
Gus Gutoski <gus.gutoski@protonmail.com>
2021-03-09 22:42:48 -0500

    verify range proof in sign r2

--- src/protocol/gg20/sign/r2.rs
+++ src/protocol/gg20/sign/r2.rs
@@ -1,5 +1,6 @@
 use super::{Sign, Status};
 use crate::fillvec::FillVec;
+use crate::zkp::RangeStatement;
 use curv::FE;
 use multi_party_ecdsa::utilities::mta;
 use serde::{Deserialize, Serialize};
@@ -26,7 +27,7 @@ impl Sign {
         // 2. my_ecdsa_nonce_summand (other) * my_secret_key_summand (me)
         // both MtAs use my_ecdsa_nonce_summand, so I use the same message for both
 
-        // TODO these variable names are getting ridiculous
+        let r1state = self.r1state.as_ref().unwrap();
         let mut out_p2ps = FillVec::with_len(self.participant_indices.len());
         let mut my_mta_blind_summands_rhs = FillVec::with_len(self.participant_indices.len());
         let mut my_mta_keyshare_summands_rhs = FillVec::with_len(self.participant_indices.len());
@@ -38,23 +39,34 @@ impl Sign {
             // TODO don't use mta!  It sucks!
             // 1. unused return values in MessageB::b()
             // 2. MessageA arg is passed by value
-            let r1state = &self.r1state.as_ref().unwrap();
-            let alice_ek = &self.my_secret_key_share.all_eks[*participant_index];
-            // let c_a = self.in_r1p2ps.vec_ref()[i]
+            let other_ek = &self.my_secret_key_share.all_eks[*participant_index];
             let c_a = self.in_r1bcasts.vec_ref()[i]
                 .as_ref()
                 .unwrap()
                 .encrypted_ecdsa_nonce_summand
                 .clone();
-
-            // TODO verify zkp
+            self.my_secret_key_share
+                .my_zkp
+                .verify_range_proof(
+                    &RangeStatement {
+                        ciphertext: &c_a.c,
+                        ek: other_ek,
+                    },
+                    &self.in_r1p2ps.vec_ref()[i].as_ref().unwrap().range_proof,
+                )
+                .unwrap_or_else(|_| {
+                    panic!(
+                        "party {} says: range proof failed to verify for party {}",
+                        self.my_secret_key_share.my_index, participant_index
+                    )
+                });
 
             let (mta_response_blind, my_mta_blind_summand_rhs, _, _) = // (m_b_gamma, beta_gamma)
-                mta::MessageB::b(&r1state.my_secret_blind_summand, alice_ek, c_a.clone());
+                mta::MessageB::b(&r1state.my_secret_blind_summand, other_ek, c_a.clone());
 
             // TODO support MtAwc! https://github.com/axelarnetwork/tofn/issues/7
             let (mta_response_keyshare, my_mta_keyshare_summand_rhs, _, _) = // (m_b_w, beta_wi)
-                mta::MessageB::b(&r1state.my_secret_key_summand, alice_ek, c_a);
+                mta::MessageB::b(&r1state.my_secret_key_summand, other_ek, c_a);
 
             // TODO I'm not sending my rhs summands even though zengo does https://github.com/axelarnetwork/tofn/issues/7#issuecomment-771379525
 
