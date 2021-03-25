a3dc722bc7f6270588d581cb902e8ba8c8a6075f
Gus Gutoski <gus.gutoski@protonmail.com>
2021-03-25 14:09:41 -0400

    tidy tests

--- src/protocol/gg20/sign/tests.rs
+++ src/protocol/gg20/sign/tests.rs
@@ -1,11 +1,8 @@
 use super::*;
-use crate::{
-    protocol::{
-        gg20::keygen::{self, SecretKeyShare},
-        tests::{execute_protocol_vec, execute_protocol_vec_self_delivery},
-        Protocol,
-    },
-    zkp::range::tests::corrupt_proof,
+use crate::protocol::{
+    gg20::keygen::{self, SecretKeyShare},
+    tests::{execute_protocol_vec, execute_protocol_vec_self_delivery},
+    Protocol,
 };
 use curv::{
     elliptic::curves::traits::{ECPoint, ECScalar},
@@ -15,8 +12,8 @@ use k256::{ecdsa::Signature, FieldBytes};
 use keygen::tests::execute_keygen;
 
 lazy_static::lazy_static! {
-    static ref MSG_TO_SIGN: Vec<u8> = vec![42];
-    static ref TEST_CASES: Vec<(usize, usize, Vec<usize>)> = vec![ // (share_count, threshold, participant_indices)
+    pub static ref MSG_TO_SIGN: Vec<u8> = vec![42];
+    pub static ref TEST_CASES: Vec<(usize, usize, Vec<usize>)> = vec![ // (share_count, threshold, participant_indices)
         // (5, 2, vec![1,2,4]),
         (5, 2, vec![4,1,2]),
         // (5, 2, vec![0,1,2,3]),
@@ -27,14 +24,18 @@ lazy_static::lazy_static! {
 }
 
 #[test]
-fn sign() {
+fn basic_correctness() {
     for (share_count, threshold, participant_indices) in TEST_CASES.iter() {
         let key_shares = execute_keygen(*share_count, *threshold);
-        execute_sign(&key_shares, participant_indices, &MSG_TO_SIGN);
+        basic_correctness_inner(&key_shares, participant_indices, &MSG_TO_SIGN);
     }
 }
 
-fn execute_sign(key_shares: &[SecretKeyShare], participant_indices: &[usize], msg_to_sign: &[u8]) {
+fn basic_correctness_inner(
+    key_shares: &[SecretKeyShare],
+    participant_indices: &[usize],
+    msg_to_sign: &[u8],
+) {
     let mut participants: Vec<Sign> = participant_indices
         .iter()
         .map(|i| Sign::new(&key_shares[*i], participant_indices, msg_to_sign).unwrap())
@@ -250,7 +251,7 @@ fn execute_sign(key_shares: &[SecretKeyShare], participant_indices: &[usize], ms
 }
 
 #[test]
-fn sign_protocol() {
+fn basic_correctness_protocol() {
     for (share_count, threshold, participant_indices) in TEST_CASES.iter() {
         let key_shares = execute_keygen(*share_count, *threshold);
 
@@ -268,7 +269,7 @@ fn sign_protocol() {
 }
 
 #[test]
-fn sign_protocol_with_self_delivery() {
+fn protocol_with_self_delivery() {
     for (share_count, threshold, participant_indices) in TEST_CASES.iter() {
         let key_shares = execute_keygen(*share_count, *threshold);
 
@@ -287,204 +288,3 @@ fn sign_protocol_with_self_delivery() {
 }
 
 // TODO move these tests into r3fail module
-#[test]
-fn sign_fault_r2_bad_proof() {
-    for (share_count, threshold, participant_indices) in TEST_CASES.iter() {
-        if *share_count < 2 {
-            continue; // need at least 2 shares for this test
-        }
-        let key_shares = execute_keygen(*share_count, *threshold);
-        execute_sign_fault_r2_bad_proof(&key_shares, participant_indices, &MSG_TO_SIGN);
-    }
-}
-
-fn execute_sign_fault_r2_bad_proof(
-    key_shares: &[SecretKeyShare],
-    participant_indices: &[usize],
-    msg_to_sign: &[u8],
-) {
-    assert!(participant_indices.len() > 1);
-    let (criminal, victim) = (1, 0);
-
-    let mut participants: Vec<Sign> = participant_indices
-        .iter()
-        .map(|i| Sign::new(&key_shares[*i], participant_indices, msg_to_sign).unwrap())
-        .collect();
-
-    // execute round 1 all participants and store their outputs
-    let mut all_r1_bcasts = FillVec::with_len(participants.len());
-    let mut all_r1_p2ps = Vec::with_capacity(participants.len());
-    for (i, participant) in participants.iter_mut().enumerate() {
-        let (state, bcast, p2ps) = participant.r1();
-        participant.r1state = Some(state);
-        participant.status = Status::R1;
-        all_r1_bcasts.insert(i, bcast).unwrap();
-        all_r1_p2ps.push(p2ps);
-    }
-
-    // corrupt the proof from party `criminal` to party `victim`
-    let proof = &mut all_r1_p2ps[criminal].vec_ref_mut()[victim]
-        .as_mut()
-        .unwrap()
-        .range_proof;
-    *proof = corrupt_proof(proof);
-
-    // deliver round 1 msgs
-    for participant in participants.iter_mut() {
-        participant.in_all_r1p2ps = all_r1_p2ps.clone();
-        participant.in_r1bcasts = all_r1_bcasts.clone();
-    }
-
-    // execute round 2 all participants and store their outputs
-    let mut all_r2_p2ps = Vec::with_capacity(participants.len());
-    let mut all_r2_bcasts_fail = FillVec::with_len(participants.len());
-    for (i, participant) in participants.iter_mut().enumerate() {
-        match participant.r2() {
-            r2::Output::Success { state, out_p2ps } => {
-                if i == victim {
-                    panic!(
-                        "r2 party {} expect failure but found success",
-                        participant.my_secret_key_share.my_index
-                    );
-                }
-                participant.r2state = Some(state);
-                all_r2_p2ps.push(out_p2ps);
-            }
-            r2::Output::Fail { out_bcast } => {
-                if i != victim {
-                    panic!(
-                        "r2 party {} expect success but found failure with culprits {:?}",
-                        participant.my_secret_key_share.my_index, out_bcast.culprits
-                    );
-                }
-                all_r2_bcasts_fail.insert(i, out_bcast).unwrap();
-                all_r2_p2ps.push(FillVec::with_len(0)); // dummy TODO use FillVec instead of Vec?
-            }
-        }
-    }
-
-    // deliver round 2 msgs
-    for participant in participants.iter_mut() {
-        participant.in_all_r2p2ps = all_r2_p2ps.clone();
-        participant.in_r2bcasts_fail = all_r2_bcasts_fail.clone();
-
-        // all participants transition to R2Fail because they all received at least one r2::FailBcast
-        participant.status = Status::R2Fail;
-    }
-
-    // execute round 2 sad path all participants and store their outputs
-    let mut all_culprit_lists = Vec::with_capacity(participants.len());
-    for participant in participants.iter_mut() {
-        let culprits = participant.r3fail();
-        participant.status = Status::Fail;
-        all_culprit_lists.push(culprits);
-    }
-
-    // TEST: everyone correctly computed the culprit list
-    let actual_culprits: Vec<usize> = vec![criminal];
-    for culprit_list in all_culprit_lists {
-        assert_eq!(culprit_list, actual_culprits);
-    }
-}
-
-#[test]
-fn sign_fault_r2_false_accusation() {
-    for (share_count, threshold, participant_indices) in TEST_CASES.iter() {
-        if *share_count < 2 {
-            continue; // need at least 2 shares for this test
-        }
-        let key_shares = execute_keygen(*share_count, *threshold);
-        execute_sign_fault_r2_false_accusation(&key_shares, participant_indices, &MSG_TO_SIGN);
-    }
-}
-
-fn execute_sign_fault_r2_false_accusation(
-    key_shares: &[SecretKeyShare],
-    participant_indices: &[usize],
-    msg_to_sign: &[u8],
-) {
-    assert!(participant_indices.len() > 1);
-    let (criminal_accuser, victim_accused) = (1, 0);
-
-    let mut participants: Vec<Sign> = participant_indices
-        .iter()
-        .map(|i| Sign::new(&key_shares[*i], participant_indices, msg_to_sign).unwrap())
-        .collect();
-
-    // execute round 1 all participants and store their outputs
-    let mut all_r1_bcasts = FillVec::with_len(participants.len());
-    let mut all_r1_p2ps = Vec::with_capacity(participants.len());
-    for (i, participant) in participants.iter_mut().enumerate() {
-        let (state, bcast, p2ps) = participant.r1();
-        participant.r1state = Some(state);
-        participant.status = Status::R1;
-        all_r1_bcasts.insert(i, bcast).unwrap();
-        all_r1_p2ps.push(p2ps);
-    }
-
-    // deliver round 1 msgs
-    for participant in participants.iter_mut() {
-        participant.in_all_r1p2ps = all_r1_p2ps.clone();
-        participant.in_r1bcasts = all_r1_bcasts.clone();
-    }
-
-    // execute round 2 all participants and store their outputs
-    let mut all_r2_p2ps = Vec::with_capacity(participants.len());
-    let mut all_r2_bcasts_fail = FillVec::with_len(participants.len());
-    for (i, participant) in participants.iter_mut().enumerate() {
-        match participant.r2() {
-            r2::Output::Success { state, out_p2ps } => {
-                // insert a false accusation by party 1 against party 0
-                if i == criminal_accuser {
-                    all_r2_bcasts_fail
-                        .insert(
-                            i,
-                            r2::FailBcast {
-                                culprits: vec![r2::Culprit {
-                                    participant_index: victim_accused,
-                                }],
-                            },
-                        )
-                        .unwrap();
-                    all_r2_p2ps.push(FillVec::with_len(0)); // dummy TODO use FillVec instead of Vec?
-                } else {
-                    participant.r2state = Some(state);
-                    all_r2_p2ps.push(out_p2ps);
-                }
-            }
-            r2::Output::Fail { out_bcast } => {
-                panic!(
-                    "r2 party {} expect success got failure with culprits: {:?}",
-                    participant.my_secret_key_share.my_index, out_bcast
-                );
-            }
-        }
-    }
-
-    // deliver round 2 msgs
-    for participant in participants.iter_mut() {
-        participant.in_all_r2p2ps = all_r2_p2ps.clone();
-        participant.in_r2bcasts_fail = all_r2_bcasts_fail.clone();
-
-        // all participants transition to R2Fail because they all received at least one r2::FailBcast
-        participant.status = Status::R2Fail;
-    }
-
-    // execute round 2 sad path all participants and store their outputs
-    let mut all_culprit_lists = Vec::with_capacity(participants.len());
-    for participant in participants.iter_mut() {
-        let culprits = participant.r3fail();
-        participant.status = Status::Fail;
-        all_culprit_lists.push(culprits);
-    }
-
-    // TEST: everyone correctly computed the culprit list
-    let actual_culprits: Vec<usize> = vec![criminal_accuser];
-    for (i, culprit_list) in all_culprit_lists.iter().enumerate() {
-        assert_eq!(
-            culprit_list, &actual_culprits,
-            "party {} unexpected culprit list",
-            i
-        );
-    }
-}
