51b428625297c185cd238a87354cb2617452c04b
Stelios Daveas <sdaveas@gmail.com>
2021-03-31 13:35:28 +0300

    Add CommonInfo and ShareInfo
    These are needed for and used by tofnd. We choose to define them in tofn
    because they contain many types that are either private within tofn's
    scope or belong to crates impored by tofn. To avoid making tofn's
    structs public and importing creates from tofnd, we only expose these
    structs that contain all types we need from tofnd.

--- src/protocol/gg20/keygen/mod.rs
+++ src/protocol/gg20/keygen/mod.rs
@@ -74,6 +74,31 @@ pub struct Keygen {
     final_output: Option<SecretKeyShare>,
 }
 
+// CommonInfo and ShareInfo only used by tofnd. We choose to define them in
+// tofn because they contain many types that are either private within tofn's
+// scope or belong to crates impored by tofn. To avoid making tofn's structs
+// public and importing creates from tofnd, we only expose these structs that
+// contain all types we need from tofnd's side.
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct CommonInfo {
+    pub threshold: usize,
+    pub ecdsa_public_key: GE,
+    pub all_ecdsa_public_key_shares: Vec<GE>,
+    pub all_eks: Vec<EncryptionKey>,
+    pub all_zkps: Vec<Zkp>,
+    pub uids: Vec<String>,
+    pub my_index: usize,
+    pub share_count: usize,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ShareInfo {
+    pub my_dk: DecryptionKey,
+    pub my_ek: EncryptionKey,
+    pub my_zkp: Zkp,
+    pub my_ecdsa_secret_key_share: FE,
+}
+
 impl Keygen {
     pub fn new(share_count: usize, threshold: usize, my_index: usize) -> Result<Self, ParamsError> {
         validate_params(share_count, threshold, my_index)?;
