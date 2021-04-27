8a87c5cfc75878eb1dc164a9114ad44cddc4f4c5
Stelios Daveas <sdaveas@gmail.com>
2021-04-27 15:49:10 +0300

    Test cases for multiple faults

--- src/protocol/gg20/sign/malicious/tests.rs
+++ src/protocol/gg20/sign/malicious/tests.rs
@@ -301,35 +301,70 @@ fn generate_self_targetting_signers() -> Vec<TestCase> {
     test_cases
 }
 
-lazy_static::lazy_static! {
-    static ref SIMPLE_TEST_CASES: Vec<TestCase> = generate_simple_test_cases();
-    static ref SELF_TARGET_TEST_CASES: Vec<TestCase> = generate_self_targetting_signers();
-    static ref TEST_CASES: Vec<TestCase> = vec![
-        TestCase{
+// TODO: Add more cases here
+fn generate_multiple_faults() -> Vec<TestCase> {
+    vec![
+        TestCase {
             share_count: 5,
             threshold: 2,
             allow_self_delivery: true,
             sign_participants: vec![
-                SignParticipant{party_index: 4, behaviour: Honest},
-                SignParticipant{party_index: 2, behaviour: Honest},
-                SignParticipant{party_index: 1, behaviour: R1BadProof{victim:0}},
+                SignParticipant {
+                    party_index: 4,
+                    behaviour: Honest,
+                },
+                SignParticipant {
+                    party_index: 2,
+                    behaviour: Honest,
+                },
+                SignParticipant {
+                    party_index: 1,
+                    behaviour: R1BadProof { victim: 0 },
+                },
             ],
-            sign_expected_criminals: vec![Criminal{index: 2, crime_type: CrimeType::Malicious}]
+            sign_expected_criminals: vec![Criminal {
+                index: 2,
+                crime_type: CrimeType::Malicious,
+            }],
         },
-        TestCase{
+        TestCase {
             share_count: 5,
             threshold: 4,
             allow_self_delivery: false,
             sign_participants: vec![
-                SignParticipant{party_index: 0, behaviour: Honest},
-                SignParticipant{party_index: 1, behaviour: R1BadProof{victim:2}},
-                SignParticipant{party_index: 2, behaviour: Honest},
-                SignParticipant{party_index: 3, behaviour: R3BadProof},
-                SignParticipant{party_index: 4, behaviour: Honest},
+                SignParticipant {
+                    party_index: 0,
+                    behaviour: Honest,
+                },
+                SignParticipant {
+                    party_index: 1,
+                    behaviour: R1BadProof { victim: 2 },
+                },
+                SignParticipant {
+                    party_index: 2,
+                    behaviour: Honest,
+                },
+                SignParticipant {
+                    party_index: 3,
+                    behaviour: R3BadProof,
+                },
+                SignParticipant {
+                    party_index: 4,
+                    behaviour: Honest,
+                },
             ],
-            sign_expected_criminals: vec![Criminal{index: 1, crime_type: CrimeType::Malicious}]
+            sign_expected_criminals: vec![Criminal {
+                index: 1,
+                crime_type: CrimeType::Malicious,
+            }],
         },
-    ];
+    ]
+}
+
+lazy_static::lazy_static! {
+    static ref SIMPLE_TEST_CASES: Vec<TestCase> = generate_simple_test_cases();
+    static ref SELF_TARGET_TEST_CASES: Vec<TestCase> = generate_self_targetting_signers();
+    static ref TEST_CASES: Vec<TestCase> = generate_multiple_faults();
 }
 
 static MESSAGE_TO_SIGN: [u8; 2] = [42, 24];
@@ -362,7 +397,9 @@ fn simple_cases() {
 #[test]
 #[traced_test]
 fn multiple_faults() {
-    for t in TEST_CASES.iter() {
+    test_cases(&TEST_CASES);
+}
+
 fn test_cases(test_cases: &[TestCase]) {
     for t in test_cases {
         let malicious_count = t
