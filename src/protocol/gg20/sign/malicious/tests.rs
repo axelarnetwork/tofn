953f61ed6decbb930d27832a1dc70857526ef43f
Stelios Daveas <sdaveas@gmail.com>
2021-04-27 15:36:29 +0300

    Simple test cases
    A non-self-targetting malicous behaviour per test. For all behaviours
    expect Honest.

--- src/protocol/gg20/sign/malicious/tests.rs
+++ src/protocol/gg20/sign/malicious/tests.rs
@@ -216,7 +216,47 @@ fn malicious_behaviour_protocol(
     }
 }
 
+use strum::IntoEnumIterator;
+// generate simple test cases using strum to iterate malicious types.
+// Strum fills all additional data on enum variants with Default::default()
+// https://docs.rs/strum_macros/0.20.1/strum_macros/derive.EnumIter.html
+// this means that all criminals in these test cases target index 0
+fn generate_simple_test_cases() -> Vec<TestCase> {
+    let mut test_cases = Vec::new();
+    let share_count = 5;
+    let threshold = 2;
+    let allow_self_delivery = true;
+    // skip honest bahaviour
+    for malicous_type in MaliciousType::iter().skip(1) {
+        test_cases.push(TestCase {
+            share_count,
+            threshold,
+            allow_self_delivery,
+            sign_participants: vec![
+                SignParticipant {
+                    party_index: 1,
+                    behaviour: Honest,
+                },
+                SignParticipant {
+                    party_index: 2,
+                    behaviour: Honest,
+                },
+                SignParticipant {
+                    party_index: 3,
+                    behaviour: malicous_type, // initialize enum values with Default::default()
+                },
+            ],
+            sign_expected_criminals: vec![Criminal {
+                index: 2,
+                crime_type: CrimeType::Malicious,
+            }],
+        })
+    }
+    test_cases
+}
+
 lazy_static::lazy_static! {
+    static ref SIMPLE_TEST_CASES: Vec<TestCase> = generate_simple_test_cases();
     static ref TEST_CASES: Vec<TestCase> = vec![
         TestCase{
             share_count: 5,
@@ -262,8 +302,16 @@ struct TestCase {
 
 #[test]
 #[traced_test]
+fn simple_cases() {
+    test_cases(&SIMPLE_TEST_CASES);
+}
+
+#[test]
+#[traced_test]
 fn multiple_faults() {
     for t in TEST_CASES.iter() {
+fn test_cases(test_cases: &[TestCase]) {
+    for t in test_cases {
         let malicious_count = t
             .sign_participants
             .iter()
