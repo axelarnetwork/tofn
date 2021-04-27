fabbdefbe16f7b5786bf656529fc7b08b81fb934
Stelios Daveas <sdaveas@gmail.com>
2021-04-27 19:29:01 +0300

    Make test case more clear

--- src/protocol/gg20/sign/malicious/tests/test_cases.rs
+++ src/protocol/gg20/sign/malicious/tests/test_cases.rs
@@ -43,21 +43,22 @@ pub fn generate_simple_test_cases() -> Vec<TestCase> {
     test_cases
 }
 
-// The conention we make is that self-targeting corruptions are skipped
+// Test all cases where malicious behaviours are skipped due to self-targeting
 #[rustfmt::skip] // skip formatting to make file more readable
 pub fn generate_skipping_cases() -> Vec<TestCase> {
+    let victim = 2; // all victims are at index 2
     let self_targeting_types = vec![
-        R1BadProof { victim: 2 },
-        R1FalseAccusation { victim: 2 },
-        R2BadMta { victim: 2 },
-        R2BadMtaWc { victim: 2 },
-        R2FalseAccusationMta { victim: 2 },
-        R2FalseAccusationMtaWc { victim: 2 },
-        // R3FalseAccusation { victim: 2 }, // this produces criminals
-        // R4FalseAccusation { victim: 2 }, // this produces criminals
-        R5BadProof { victim: 2 },
-        R5FalseAccusation { victim: 2 },
-        // R6FalseAccusation { victim: 2 }, // this produces criminals
+        R1BadProof { victim },
+        R1FalseAccusation { victim },
+        R2BadMta { victim },
+        R2BadMtaWc { victim },
+        R2FalseAccusationMta { victim },
+        R2FalseAccusationMtaWc { victim },
+        // R3FalseAccusation { victim }, // this produces criminals
+        // R4FalseAccusation { victim }, // this produces criminals
+        R5BadProof { victim },
+        R5FalseAccusation { victim },
+        // R6FalseAccusation { victim }, // this produces criminals
     ];
 
     let mut test_cases = Vec::new();
@@ -68,9 +69,10 @@ pub fn generate_skipping_cases() -> Vec<TestCase> {
         test_cases.push(TestCase {
             share_count, threshold, allow_self_delivery,
             sign_participants: vec![
-                SignParticipant { party_index: 1, behaviour: Honest, },
-                SignParticipant { party_index: 2, behaviour: Honest, },
-                SignParticipant { party_index: 3, behaviour: malicous_type, },
+                SignParticipant { party_index: 1, behaviour: Honest, }, // index 0
+                SignParticipant { party_index: 2, behaviour: Honest, }, // index 1
+                // all malicious parties are at index 2 and are targeting themselves 
+                SignParticipant { party_index: 3, behaviour: malicous_type, }, // index 2
             ],
             sign_expected_criminals: vec![],
         })
