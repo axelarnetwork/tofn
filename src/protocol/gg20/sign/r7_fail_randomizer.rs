f2ad41e08cf2303d985b1617411534c3f8914c90
Gus Gutoski <gus.gutoski@protonmail.com>
2021-04-29 12:47:22 -0400

    recover lhs encryption randomness

--- src/protocol/gg20/sign/r7_fail_randomizer.rs
+++ src/protocol/gg20/sign/r7_fail_randomizer.rs
@@ -1,8 +1,13 @@
 use super::{Sign, Status};
 use crate::fillvec::FillVec;
-use curv::{BigInt, FE};
+use curv::{elliptic::curves::traits::ECScalar, BigInt, FE};
+use paillier::{
+    // DecryptionKey, EncryptionKey, Open, Paillier, Randomness, RawCiphertext, RawPlaintext,
+    Open,
+    Paillier,
+    RawCiphertext,
+};
 use serde::{Deserialize, Serialize};
-
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Bcast {
     // TODO do I also need encryption randomness for alpha_ij ?  Yes.
@@ -21,6 +26,7 @@ pub struct MtaBlindSummandsData {
     pub(super) rhs: FE,                // beta_ji
     pub(super) rhs_randomness: BigInt, // beta_ji encryption randomness
     pub(super) lhs: FE,                // alpha_ij
+    pub(super) lhs_randomness: BigInt, // alpha_ij encryption randomness
 }
 
 impl Sign {
@@ -38,6 +44,29 @@ impl Sign {
             if i == self.my_participant_index {
                 continue;
             }
+
+            // recover encryption randomness for my_mta_blind_summands_lhs
+            // need to decrypt again to do so
+            let in_p2p = self.in_all_r2p2ps[i].vec_ref()[self.my_participant_index]
+                .as_ref()
+                .unwrap_or_else(|| {
+                    // TODO these checks should not be necessary after refactoring
+                    panic!(
+                        "r7_fail_randomizer participant {} says: missing r2p2p from {}",
+                        self.my_participant_index, i
+                    )
+                });
+            let (my_mta_blind_summand_lhs, my_mta_blind_summand_lhs_randomness) = Paillier::open(
+                &self.my_secret_key_share.my_dk,
+                &RawCiphertext::from(&in_p2p.mta_response_blind.c),
+            );
+            let my_mta_blind_summand_lhs: FE = ECScalar::from(&my_mta_blind_summand_lhs.0);
+            assert_eq!(
+                my_mta_blind_summand_lhs,
+                r3state.my_mta_blind_summands_lhs[i].unwrap(),
+                "participant {}: decryption of mta_response_blind from {} in r7_fail_randomizer differs from r3", self.my_participant_index, i
+            ); // TODO panic
+
             mta_blind_summands
                 .insert(
                     i,
@@ -47,7 +76,8 @@ impl Sign {
                             .as_ref()
                             .unwrap()
                             .clone(),
-                        lhs: r3state.my_mta_blind_summands_lhs[i].unwrap(),
+                        lhs: my_mta_blind_summand_lhs,
+                        lhs_randomness: my_mta_blind_summand_lhs_randomness.0,
                     },
                 )
                 .unwrap();
