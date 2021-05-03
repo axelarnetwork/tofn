afcdaeb8531bd4f3b2019e22ffd52a20c98b6334
Stelios Daveas <48155711+sdaveas@users.noreply.github.com>
2021-05-03 21:34:14 +0300

    test(migrate): complete-criminal-tests (#47)
    * Add test cases for multiple faults in the same round
    
    * Add test cases for multiple victims in the same round
    
    * Add test cases for multiple faults
    
    * Add test cases for exceeding threshold
    
    * Add test cases for invalid index
    
    * cargo fmt
    
    but why?

--- src/protocol/gg20/sign/malicious/tests/mod.rs
+++ src/protocol/gg20/sign/malicious/tests/mod.rs
@@ -157,6 +157,11 @@ mod test_cases2;
 lazy_static::lazy_static! {
     static ref BASIC_CASES_2: Vec<test_cases2::TestCase> = test_cases2::generate_basic_cases();
     static ref SKIPPING_CASES_2: Vec<test_cases2::TestCase> = test_cases2::generate_skipping_cases_2();
+    static ref SAME_ROUND_CASES_2: Vec<test_cases2::TestCase> = test_cases2::generate_multiple_faults_in_same_round_2();
+    static ref MULTIPLE_VICTIMS_2: Vec<test_cases2::TestCase> = test_cases2::generate_target_multiple_parties_2();
+    static ref MULTIPLE_FAULTS_2: Vec<test_cases2::TestCase> = test_cases2::generate_multiple_faults_2();
+    static ref PANIC_THRESHOLD_2: Vec<test_cases2::TestCase> = test_cases2::generate_small_threshold_2();
+    static ref PANIC_INDEX_2: Vec<test_cases2::TestCase> = test_cases2::generate_out_of_index_2();
 }
 
 #[test]
@@ -169,6 +174,36 @@ fn basic_tests_2() {
 #[traced_test]
 fn skipping_cases_2() {
     execute_test_case_list_2(&SKIPPING_CASES_2);
+}
+
+#[test]
+#[traced_test]
+fn same_round_cases_2() {
+    execute_test_case_list_2(&SAME_ROUND_CASES_2);
+}
+
+#[test]
+#[traced_test]
+fn multiple_targets_cases_2() {
+    execute_test_case_list_2(&MULTIPLE_VICTIMS_2);
+}
+
+#[test]
+#[traced_test]
+fn multiple_faults_2() {
+    execute_test_case_list_2(&MULTIPLE_FAULTS_2);
+}
+
+#[test]
+#[should_panic]
+fn panic_small_threshold_2() {
+    execute_test_case_list_2(&PANIC_THRESHOLD_2);
+}
+
+#[test]
+#[should_panic]
+fn panic_out_of_index_2() {
+    execute_test_case_list_2(&PANIC_INDEX_2);
 }
 
 fn execute_test_case_list_2(test_cases: &[test_cases2::TestCase]) {
--- src/protocol/gg20/sign/malicious/tests/test_cases2.rs
+++ src/protocol/gg20/sign/malicious/tests/test_cases2.rs
@@ -130,3 +130,174 @@ pub(super) fn generate_skipping_cases_2() -> Vec<TestCase> {
     }
     test_cases
 }
+
+pub(super) fn generate_multiple_faults_in_same_round_2() -> Vec<TestCase> {
+    // list all bad behaviours per round
+    // I wish all faults of the round X would have a common prefix R'X' so I could use
+    // let r1_faults = MaliciousType:iter().filter(|type| type.contains("R1"));
+    // instead of manually listing them
+    let victim = 0;
+    let all_rounds_faults = vec![
+        // round 1 faults
+        vec![R1BadProof { victim }, R2FalseAccusation { victim }],
+        // round 2 faults
+        vec![
+            R2BadMta { victim },
+            R2BadMtaWc { victim },
+            R3FalseAccusationMta { victim },
+            R3FalseAccusationMtaWc { victim },
+        ],
+        // round 3 faults
+        vec![R3BadProof],
+        // round 4 faults
+        vec![R4BadReveal],
+        // round 5 faults
+        vec![R5BadProof { victim }, R6FalseAccusation { victim }],
+        // round 6 faults
+        vec![R6BadProof],
+        // round 7 faults
+        vec![R7BadSigSummand],
+    ];
+
+    // create test cases for all rounds
+    let mut test_cases = Vec::new();
+    for round_faults in all_rounds_faults {
+        // start with the victim at pos 0
+        let mut participants = vec![SignParticipant {
+            party_index: round_faults.len(), // give the good guy the last party index
+            behaviour: Honest,
+            expected_crimes: vec![],
+        }];
+        for (i, fault) in round_faults.into_iter().enumerate() {
+            participants.push(
+                // I have to state `expected_crimes` before `behaviour` because
+                // the later consumes `fault` and I cannot borrow it after that
+                // wonder if rust compiler could accomodate that
+                SignParticipant {
+                    party_index: i,
+                    expected_crimes: map_type_to_crime(&fault),
+                    behaviour: fault, // behaviour data initialized by Default:default()
+                },
+            );
+        }
+        test_cases.push(TestCase {
+            share_count: 5,
+            threshold: participants.len() - 1, // threshold < #parties
+            allow_self_delivery: true,
+            expect_success: false,
+            sign_participants: participants,
+        });
+    }
+    test_cases
+}
+
+#[rustfmt::skip] // skip formatting to make file more readable
+pub(super) fn generate_target_multiple_parties_2() -> Vec<TestCase> {
+    vec![
+        TestCase {
+            share_count: 9, threshold: 6, allow_self_delivery: true, expect_success: false,
+            sign_participants: vec![
+                SignParticipant { party_index: 0, behaviour: Honest, expected_crimes: vec![]},
+                SignParticipant { party_index: 1, behaviour: Honest, expected_crimes: vec![]},
+                SignParticipant { party_index: 2, behaviour: Honest, expected_crimes: vec![]},
+                SignParticipant { party_index: 3, behaviour: R1BadProof { victim: 0 }, expected_crimes: map_type_to_crime(&R1BadProof{victim:0})},
+                SignParticipant { party_index: 4, behaviour: R2FalseAccusation{ victim: 0}, expected_crimes: map_type_to_crime(&R2FalseAccusation{ victim: 0})},
+                SignParticipant { party_index: 5, behaviour: R1BadProof { victim: 1 }, expected_crimes: map_type_to_crime(&R1BadProof{ victim: 1})},
+                SignParticipant { party_index: 6, behaviour: R2FalseAccusation{ victim: 1}, expected_crimes: map_type_to_crime(&R2FalseAccusation{ victim: 1})},
+                // R5 should not be registered because they happen after R1 crimes
+                SignParticipant { party_index: 7, behaviour: R5BadProof { victim: 2 }, expected_crimes: vec![]},
+                SignParticipant { party_index: 8, behaviour: R6FalseAccusation{ victim: 2}, expected_crimes: vec![]},
+            ],
+        },
+    ]
+}
+
+#[rustfmt::skip] // skip formatting to make file more readable
+pub(super) fn generate_multiple_faults_2() -> Vec<TestCase> {
+    vec![
+        TestCase {
+            share_count: 5, threshold: 4, allow_self_delivery: false, expect_success: false,
+            sign_participants: vec![
+                SignParticipant { party_index: 0, behaviour: Honest, expected_crimes: vec![]},
+                SignParticipant { party_index: 1, behaviour: R1BadProof { victim: 2 }, expected_crimes: map_type_to_crime(&R1BadProof{victim:2})},
+                SignParticipant { party_index: 2, behaviour: Honest, expected_crimes: vec![]},
+                SignParticipant { party_index: 3, behaviour: R3BadProof, expected_crimes: vec![]},
+                SignParticipant { party_index: 4, behaviour: Honest, expected_crimes: vec![]},
+            ],
+        },
+        TestCase {
+            share_count: 10, threshold: 4, allow_self_delivery: false, expect_success: false,
+            sign_participants: vec![
+                SignParticipant { party_index: 0, behaviour: R1BadProof { victim: 3 }, expected_crimes: map_type_to_crime(&R1BadProof{victim: 3})},
+                SignParticipant { party_index: 1, behaviour: R2BadMta{victim: 3}, expected_crimes: vec![]},
+                SignParticipant { party_index: 2, behaviour: R3BadProof, expected_crimes: vec![]},
+                SignParticipant { party_index: 3, behaviour: Honest, expected_crimes: vec![]},
+                SignParticipant { party_index: 4, behaviour: R4BadReveal, expected_crimes: vec![]},
+                SignParticipant { party_index: 5, behaviour: R5BadProof{victim: 3}, expected_crimes: vec![]},
+                SignParticipant { party_index: 6, behaviour: R6BadProof, expected_crimes: vec![]},
+                SignParticipant { party_index: 7, behaviour: R7BadSigSummand, expected_crimes: vec![]},
+            ],
+        },
+        TestCase {
+            share_count: 10, threshold: 4, allow_self_delivery: false, expect_success: false,
+            sign_participants: vec![
+                SignParticipant { party_index: 0, behaviour: Honest, expected_crimes: vec![]},
+                SignParticipant { party_index: 1, behaviour: R1BadProof { victim: 0 }, expected_crimes: map_type_to_crime(&R1BadProof{victim: 0})},
+                SignParticipant { party_index: 2, behaviour: R2BadMta{victim: 1}, expected_crimes: vec![]},
+                SignParticipant { party_index: 3, behaviour: R3BadProof, expected_crimes: vec![]},
+                SignParticipant { party_index: 4, behaviour: R4BadReveal, expected_crimes: vec![]},
+                SignParticipant { party_index: 5, behaviour: R5BadProof{victim: 3}, expected_crimes: vec![]},
+                SignParticipant { party_index: 6, behaviour: R6BadProof, expected_crimes: vec![]},
+                SignParticipant { party_index: 7, behaviour: R7BadSigSummand, expected_crimes: vec![]},
+            ],
+        },
+    ]
+}
+
+// Threshold is equal to the number of participants
+pub(super) fn generate_small_threshold_2() -> Vec<TestCase> {
+    vec![TestCase {
+        share_count: 5,
+        threshold: 4,
+        allow_self_delivery: false, // panic: threashold is 4, signers are 4
+        expect_success: false,
+        sign_participants: vec![
+            SignParticipant {
+                party_index: 0,
+                behaviour: Honest,
+                expected_crimes: vec![],
+            },
+            SignParticipant {
+                party_index: 1,
+                behaviour: Honest,
+                expected_crimes: vec![],
+            },
+            SignParticipant {
+                party_index: 2,
+                behaviour: Honest,
+                expected_crimes: vec![],
+            },
+            SignParticipant {
+                party_index: 3,
+                behaviour: Honest,
+                expected_crimes: vec![],
+            },
+        ],
+    }]
+}
+
+// Target a party that does not exist
+#[rustfmt::skip] // skip formatting to make file more readable
+pub(super) fn generate_out_of_index_2() -> Vec<TestCase> {
+    vec![
+        TestCase {
+            share_count: 5, threshold: 4, allow_self_delivery: false, expect_success: false,
+            sign_participants: vec![
+                SignParticipant { party_index: 0, behaviour: Honest, expected_crimes: vec![]},
+                SignParticipant { party_index: 1, behaviour: Honest, expected_crimes: vec![]},
+                SignParticipant { party_index: 2, behaviour: Honest, expected_crimes: vec![]},
+                SignParticipant { party_index: 5, behaviour: Honest, expected_crimes: vec![]}, // panic: index is equal to share_counts
+            ],
+        },
+    ]
+}
