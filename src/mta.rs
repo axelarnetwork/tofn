d69575d63a96614022e574ea817349d2f763e04d
Gus Gutoski <gus.gutoski@protonmail.com>
2021-05-27 14:00:31 -0400

    add mta_response_with_proof_wc

--- src/mta.rs
+++ src/mta.rs
@@ -3,11 +3,6 @@ use crate::paillier_k256::{
     Ciphertext, EncryptionKey, Plaintext, Randomness,
 };
 
-pub(crate) struct Public {
-    c_b: Ciphertext,
-    proof: mta::Proof,
-}
-
 pub(crate) struct Secret {
     beta: k256::Scalar,
     beta_prime: Plaintext,
@@ -15,33 +10,19 @@ pub(crate) struct Secret {
 }
 
 pub(crate) fn mta_response(
-    a_zkp: &ZkSetup,
     a_ek: &EncryptionKey,
     a_ciphertext: &Ciphertext,
     b: &k256::Scalar,
-) -> (Public, Secret) {
+) -> (Ciphertext, Secret) {
     let beta_prime = a_ek.random_plaintext();
     let (beta_prime_ciphertext, beta_prime_randomness) = a_ek.encrypt(&beta_prime);
     let c_b = a_ek.add(
         &a_ek.mul(a_ciphertext, &Plaintext::from_scalar(b)),
         &beta_prime_ciphertext,
     );
-    let proof = a_zkp.mta_proof(
-        &mta::Statement {
-            ciphertext1: a_ciphertext,
-            ciphertext2: &c_b,
-            ek: a_ek,
-        },
-        &mta::Witness {
-            x: b,
-            msg: &beta_prime,
-            randomness: &beta_prime_randomness,
-        },
-    );
     let beta = beta_prime.to_scalar().negate();
-
     (
-        Public { c_b, proof },
+        c_b,
         Secret {
             beta,
             beta_prime,
@@ -50,11 +31,59 @@ pub(crate) fn mta_response(
     )
 }
 
+pub(crate) fn mta_response_with_proof(
+    a_zkp: &ZkSetup,
+    a_ek: &EncryptionKey,
+    a_ciphertext: &Ciphertext,
+    b: &k256::Scalar,
+) -> (Ciphertext, mta::Proof, Secret) {
+    let (c_b, s) = mta_response(a_ek, a_ciphertext, b);
+    let proof = a_zkp.mta_proof(
+        &mta::Statement {
+            ciphertext1: a_ciphertext,
+            ciphertext2: &c_b,
+            ek: a_ek,
+        },
+        &mta::Witness {
+            x: b,
+            msg: &s.beta_prime,
+            randomness: &s.beta_prime_randomness,
+        },
+    );
+    (c_b, proof, s)
+}
+
+pub(crate) fn mta_response_with_proof_wc(
+    a_zkp: &ZkSetup,
+    a_ek: &EncryptionKey,
+    a_ciphertext: &Ciphertext,
+    b: &k256::Scalar,
+    b_g: &k256::ProjectivePoint,
+) -> (Ciphertext, mta::ProofWc, Secret) {
+    let (c_b, s) = mta_response(a_ek, a_ciphertext, b);
+    let proof_wc = a_zkp.mta_proof_wc(
+        &mta::StatementWc {
+            stmt: mta::Statement {
+                ciphertext1: a_ciphertext,
+                ciphertext2: &c_b,
+                ek: a_ek,
+            },
+            x_g: b_g,
+        },
+        &mta::Witness {
+            x: b,
+            msg: &s.beta_prime,
+            randomness: &s.beta_prime_randomness,
+        },
+    );
+    (c_b, proof_wc, s)
+}
+
 #[cfg(test)]
 pub(crate) mod tests {
     use ecdsa::elliptic_curve::Field;
 
-    use super::mta_response;
+    use super::mta_response_with_proof_wc;
     use crate::paillier_k256::{
         keygen_unsafe,
         zk::{mta, range, ZkSetup},
@@ -64,6 +93,7 @@ pub(crate) mod tests {
     fn basic_correctness() {
         let a = k256::Scalar::random(rand::thread_rng());
         let b = k256::Scalar::random(rand::thread_rng());
+        let b_g = k256::ProjectivePoint::generator() * b;
         let (a_ek, a_dk) = keygen_unsafe();
         let a_zkp = ZkSetup::new_unsafe();
         let b_zkp = ZkSetup::new_unsafe();
@@ -91,20 +121,24 @@ pub(crate) mod tests {
                 &a_range_proof,
             )
             .unwrap();
-        let (b_public, b_secret) = mta_response(&a_zkp, &a_ek, &a_ciphertext, &b);
+        let (c_b, b_mta_proof_wc, b_secret) =
+            mta_response_with_proof_wc(&a_zkp, &a_ek, &a_ciphertext, &b, &b_g);
 
         // MtA step 3: party a
         a_zkp
-            .verify_mta_proof(
-                &mta::Statement {
-                    ciphertext1: &a_ciphertext,
-                    ciphertext2: &b_public.c_b,
-                    ek: &a_ek,
+            .verify_mta_proof_wc(
+                &mta::StatementWc {
+                    stmt: mta::Statement {
+                        ciphertext1: &a_ciphertext,
+                        ciphertext2: &c_b,
+                        ek: &a_ek,
+                    },
+                    x_g: &b_g,
                 },
-                &b_public.proof,
+                &b_mta_proof_wc,
             )
             .unwrap();
-        let alpha = a_dk.decrypt_with_randomness(&b_public.c_b).0.to_scalar();
+        let alpha = a_dk.decrypt_with_randomness(&c_b).0.to_scalar();
 
         // test: a * b = alpha + beta
         assert_eq!(a * b, alpha + b_secret.beta);
