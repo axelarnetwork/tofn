8a26180c2b1ebe1ada7e9322cb23da94cfe30c1b
Gus Gutoski <gus@axelar.network>
2021-06-03 14:44:22 -0400

    r7_fail, r7_fail_type5

--- src/protocol/gg20/sign/r7_fail.rs
+++ src/protocol/gg20/sign/r7_fail.rs
@@ -1,6 +1,5 @@
 use super::{crimes::Crime, Sign, Status};
 use crate::paillier_k256::zk;
-use crate::zkp::paillier::range;
 use tracing::info;
 
 impl Sign {
@@ -15,6 +14,7 @@ impl Sign {
         // TODO clarify confusion: participant vs party indices
         for accuser in 0..self.participant_indices.len() {
             if let Some(fail_bcast) = self.in_r6bcasts_fail.vec_ref()[accuser].as_ref() {
+                // TODO prevent duplicate complaints
                 for accused in fail_bcast.culprits.iter() {
                     if accuser == accused.participant_index {
                         let crime = Crime::R7FailFalseAccusation { victim: accuser };
@@ -25,37 +25,6 @@ impl Sign {
                         criminals[accuser].push(crime);
                         continue;
                     }
-
-                    // curv: check proof
-                    let prover_ek = &self.my_secret_key_share.all_eks
-                        [self.participant_indices[accused.participant_index]];
-                    let prover_encrypted_ecdsa_nonce_summand = &self.in_r1bcasts.vec_ref()
-                        [accused.participant_index]
-                        .as_ref()
-                        .unwrap()
-                        .k_i_ciphertext
-                        .c;
-                    let prover_ecdsa_randomizer_x_nonce_summand = &self.in_r5bcasts.vec_ref()
-                        [accused.participant_index]
-                        .as_ref()
-                        .unwrap()
-                        .R_i;
-                    let ecdsa_randomizer = &self.r5state.as_ref().unwrap().R;
-                    let verifier_zkp =
-                        &self.my_secret_key_share.all_zkps[self.participant_indices[accuser]];
-                    let stmt = &range::StatementWc {
-                        stmt: range::Statement {
-                            ciphertext: prover_encrypted_ecdsa_nonce_summand,
-                            ek: prover_ek,
-                        },
-                        msg_g: prover_ecdsa_randomizer_x_nonce_summand,
-                        g: ecdsa_randomizer,
-                    };
-                    let proof = &self.in_all_r5p2ps[accused.participant_index].vec_ref()[accuser]
-                        .as_ref()
-                        .unwrap()
-                        .k_i_range_proof_wc;
-                    let verification = verifier_zkp.verify_range_proof_wc(stmt, proof);
 
                     // k256: check proof
                     let prover_ek_k256 = &self.my_secret_key_share.all_eks_k256
@@ -85,10 +54,8 @@ impl Sign {
                         .as_ref()
                         .unwrap()
                         .k_i_range_proof_wc_k256;
-                    let verification_k256 =
-                        verifier_zkp_k256.verify_range_proof_wc(stmt_k256, proof_k256);
 
-                    match verification_k256 {
+                    match verifier_zkp_k256.verify_range_proof_wc(stmt_k256, proof_k256) {
                         Ok(_) => {
                             let crime = Crime::R7FailFalseAccusation {
                                 victim: accused.participant_index,
--- src/protocol/gg20/sign/r7_fail_type5.rs
+++ src/protocol/gg20/sign/r7_fail_type5.rs
@@ -1,12 +1,6 @@
 use super::{crimes::Crime, Sign, Status};
 use crate::mta;
 use crate::paillier_k256::Plaintext;
-use curv::{
-    elliptic::curves::traits::{ECPoint, ECScalar},
-    FE, GE,
-};
-use multi_party_ecdsa::utilities::mta as mta_zengo;
-use paillier::{EncryptWithChosenRandomness, Paillier, Randomness, RawPlaintext};
 use tracing::{error, info, warn};
 
 impl Sign {
@@ -35,31 +29,6 @@ impl Sign {
             // verify correct computation of delta_i
             // as per page 17 of https://eprint.iacr.org/2020/540.pdf doc version 20200511:155431
             let in_r3bcast = self.in_r3bcasts.vec_ref()[i].as_ref().unwrap();
-
-            // curv
-            let mut delta_i = r6_participant_data
-                .k_i
-                .mul(&r6_participant_data.gamma_i.get_element()); // k_i * gamma_i
-            for (j, mta_plaintext) in r6_participant_data.mta_plaintexts.iter().enumerate() {
-                if j == i {
-                    continue;
-                }
-                let mta_plaintext = mta_plaintext.as_ref().unwrap();
-                let alpha: FE = ECScalar::from(&mta_plaintext.alpha_plaintext);
-                delta_i = delta_i + alpha + mta_plaintext.beta; // alpha_ij + beta_ji
-            }
-            if delta_i != in_r3bcast.delta_i {
-                let crime = Crime::R7FailType5BadDeltaI;
-                info!(
-                    "(curv) participant {} detect {:?} by {}",
-                    self.my_participant_index, crime, i
-                );
-                criminals[i].push(crime);
-                // TODO continue looking for more crimes by this participant?
-                // continue; // participant i is known to be criminal, continue to next participant
-            }
-
-            // k256
             let delta_i_k256 = {
                 let mut sum = r6_participant_data.k_i_256.unwrap()
                     * r6_participant_data.gamma_i_k256.unwrap();
@@ -85,33 +54,14 @@ impl Sign {
             }
 
             // verify r7_participant_data is consistent with earlier messages:
-            // 1. ecdsa_nonce_summand (k_i)
-            // 2. secret_blind_summand (gamma_i)
-            // 3. mta_blind_summands.rhs (beta_ij)
-            // 4. mta_blind_summands.lhs (alpha_ij)
+            // 1. k_i
+            // 2. gamma_i
+            // 3. beta_ij
+            // 4. alpha_ij
             let in_r1bcast = self.in_r1bcasts.vec_ref()[i].as_ref().unwrap();
-
-            // curv: 1. k_i
-            let ek = &self.my_secret_key_share.all_eks[self.participant_indices[i]];
-            let encrypted_ecdsa_nonce_summand = Paillier::encrypt_with_chosen_randomness(
-                ek,
-                RawPlaintext::from(r6_participant_data.k_i.to_big_int()),
-                &Randomness::from(&r6_participant_data.k_i_randomness),
-            );
-            if *encrypted_ecdsa_nonce_summand.0 != in_r1bcast.k_i_ciphertext.c {
-                // this code path triggered by R3BadEcdsaNonceSummand
-                let crime = Crime::R7FailType5BadKI;
-                info!(
-                    "(curv) participant {} detect {:?} by {}",
-                    self.my_participant_index, crime, i
-                );
-                criminals[i].push(crime);
-                // TODO continue looking for more crimes?
-                // continue; // participant i is known to be criminal, continue to next participant
-            }
-
-            // k256: 1. k_i
             let ek_k256 = &self.my_secret_key_share.all_eks_k256[self.participant_indices[i]];
+
+            // k_i
             let k_i_ciphertext_k256 = ek_k256.encrypt_with_randomness(
                 &Plaintext::from_scalar(r6_participant_data.k_i_256.unwrap()),
                 &r6_participant_data.k_i_randomness_k256,
@@ -127,28 +77,7 @@ impl Sign {
                 // continue; // participant i is known to be criminal, continue to next participant
             }
 
-            // curv: 2. gamma_i
-            let public_blind_summand = GE::generator() * r6_participant_data.gamma_i;
-            let in_r4bcast = self.in_r4bcasts.vec_ref()[i].as_ref().unwrap_or_else(|| {
-                panic!(
-                    // TODO these checks should be unnecessary after refactoring
-                    "r7_fail_type5 participant {} missing in_r4bcast from {}",
-                    self.my_participant_index, i
-                )
-            });
-            if public_blind_summand != in_r4bcast.Gamma_i {
-                // this code path triggered by R1BadSecretBlindSummand
-                let crime = Crime::R7FailType5BadGammaI;
-                info!(
-                    "(curv) participant {} detect {:?} by {}",
-                    self.my_participant_index, crime, i
-                );
-                criminals[i].push(crime);
-                // TODO continue looking for more crimes?
-                // continue; // participant i is known to be criminal, continue to next participant
-            }
-
-            // k256: 2. gamma_i
+            // gamma_i
             let Gamma_i_k256 =
                 k256::ProjectivePoint::generator() * r6_participant_data.gamma_i_k256.unwrap();
             let in_r4bcast = self.in_r4bcasts.vec_ref()[i].as_ref().unwrap();
@@ -163,48 +92,14 @@ impl Sign {
                 // continue; // participant i is known to be criminal, continue to next participant
             }
 
-            // 3. beta_ij
-            // 4. alpha_ij
+            // beta_ij, alpha_ij
             for (j, mta_plaintext) in r6_participant_data.mta_plaintexts.iter().enumerate() {
                 if j == i {
                     continue;
                 }
                 let mta_plaintext = mta_plaintext.as_ref().unwrap();
 
-                // curv: 3. beta_ij
-                let other_ek = &self.my_secret_key_share.all_eks[self.participant_indices[j]];
-                let k_i_ciphertext = &self.in_r1bcasts.vec_ref()[j]
-                    .as_ref()
-                    .unwrap()
-                    .k_i_ciphertext;
-                let (mta_response_blind, mta_blind_summand_rhs) = // (enc(alpha_ij), beta_ji)
-                    mta_zengo::MessageB::b_with_predefined_randomness(
-                        &r6_participant_data.gamma_i,
-                        other_ek,
-                        k_i_ciphertext.clone(),
-                        &mta_plaintext.beta_randomness.randomness,
-                        &mta_plaintext.beta_randomness.beta_prime,
-                    );
-                if mta_blind_summand_rhs != mta_plaintext.beta
-                    || mta_response_blind.c
-                        != self.in_all_r2p2ps[i].vec_ref()[j]
-                            .as_ref()
-                            .unwrap()
-                            .alpha_ciphertext
-                            .c
-                {
-                    // this code path triggered by R3BadMtaBlindSummandRhs
-                    let crime = Crime::R7FailType5BadBeta { victim: j };
-                    info!(
-                        "(curv) participant {} detect {:?} (beta_ji) by {}",
-                        self.my_participant_index, crime, i
-                    );
-                    criminals[i].push(crime);
-                    // TODO continue looking for more crimes?
-                    // continue 'outer; // participant i is known to be criminal, continue to next participant
-                }
-
-                // k256: 3. beta_ij
+                // beta_ij
                 let other_ek_k256 =
                     &self.my_secret_key_share.all_eks_k256[self.participant_indices[j]];
                 let other_k_i_ciphertext_k256 = &self.in_r1bcasts.vec_ref()[j]
@@ -232,31 +127,7 @@ impl Sign {
                     // continue 'outer; // participant i is known to be criminal, continue to next participant
                 }
 
-                // curv: 4. alpha_ij
-                let mta_blind_summand_lhs_ciphertext = Paillier::encrypt_with_chosen_randomness(
-                    ek,
-                    RawPlaintext::from(&mta_plaintext.alpha_plaintext),
-                    &Randomness::from(&mta_plaintext.alpha_randomness),
-                );
-                if *mta_blind_summand_lhs_ciphertext.0
-                    != self.in_all_r2p2ps[j].vec_ref()[i]
-                        .as_ref()
-                        .unwrap()
-                        .alpha_ciphertext
-                        .c
-                {
-                    // this code path triggered by R3BadMtaBlindSummandLhs
-                    let crime = Crime::R7FailType5BadAlpha { victim: j };
-                    info!(
-                        "(curv) participant {} detect {:?} (alpha_ij) by {}",
-                        self.my_participant_index, crime, i
-                    );
-                    criminals[i].push(crime);
-                    // TODO continue looking for more crimes?
-                    // continue 'outer; // participant i is known to be criminal, continue to next participant
-                }
-
-                // k256: 4. alpha_ij
+                // alpha_ij
                 let alpha_ciphertext_k256 = ek_k256.encrypt_with_randomness(
                     &mta_plaintext.alpha_plaintext_k256,
                     &mta_plaintext.alpha_randomness_k256,
