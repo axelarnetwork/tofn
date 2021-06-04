3bd8e9fe5eab7f404e9a8d91d8918e52c7b4f56c
Gus Gutoski <gus@axelar.network>
2021-06-04 08:51:36 -0400

    sign r3_fail

--- src/protocol/gg20/sign/r3_fail.rs
+++ src/protocol/gg20/sign/r3_fail.rs
@@ -1,6 +1,5 @@
 use super::{crimes::Crime, Sign, Status};
 use crate::paillier_k256::zk;
-use crate::zkp::paillier::range;
 use tracing::info;
 
 impl Sign {
@@ -12,6 +11,7 @@ impl Sign {
 
         for accuser in 0..self.participant_indices.len() {
             if let Some(fail_bcast) = self.in_r2bcasts_fail.vec_ref()[accuser].as_ref() {
+                // TODO don't allow duplicate complaints
                 for accused in fail_bcast.culprits.iter() {
                     if accuser == accused.participant_index {
                         let crime = Crime::R3FailFalseAccusation { victim: accuser };
@@ -23,30 +23,7 @@ impl Sign {
                         continue;
                     }
 
-                    // TODO clarify confusion: participant vs party indices
-
-                    // curv: check proof
-                    let prover_ek = &self.my_secret_key_share.all_eks
-                        [self.participant_indices[accused.participant_index]];
-                    let prover_encrypted_ecdsa_nonce_summand = &self.in_r1bcasts.vec_ref()
-                        [accused.participant_index]
-                        .as_ref()
-                        .unwrap()
-                        .k_i_ciphertext
-                        .c;
-                    let verifier_zkp =
-                        &self.my_secret_key_share.all_zkps[self.participant_indices[accuser]];
-                    let stmt = &range::Statement {
-                        ciphertext: &prover_encrypted_ecdsa_nonce_summand,
-                        ek: prover_ek,
-                    };
-                    let proof = &self.in_all_r1p2ps[accused.participant_index].vec_ref()[accuser]
-                        .as_ref()
-                        .unwrap()
-                        .range_proof;
-                    let verification = verifier_zkp.verify_range_proof(stmt, proof);
-
-                    // k256: check proof
+                    // check proof
                     let prover_ek_k256 = &self.my_secret_key_share.all_eks_k256
                         [self.participant_indices[accused.participant_index]];
                     let prover_k_i_ciphertext = &self.in_r1bcasts.vec_ref()
@@ -65,10 +42,8 @@ impl Sign {
                         .as_ref()
                         .unwrap()
                         .range_proof_k256;
-                    let verification_k256 =
-                        verifier_zkp_k256.verify_range_proof(stmt_k256, proof_k256);
 
-                    match verification_k256 {
+                    match verifier_zkp_k256.verify_range_proof(stmt_k256, proof_k256) {
                         Ok(_) => {
                             let crime = Crime::R3FailFalseAccusation {
                                 victim: accused.participant_index,
