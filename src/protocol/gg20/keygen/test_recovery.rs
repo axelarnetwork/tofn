861301bdb5fa463f928802054013c26971d33f51
Gus Gutoski <gus@axelar.network>
2021-06-18 12:03:09 -0400

    test repeated secret recovery keys

--- src/protocol/gg20/keygen/test_recovery.rs
+++ src/protocol/gg20/keygen/test_recovery.rs
@@ -4,12 +4,25 @@ use super::{tests_k256::*, *};
 
 #[test]
 fn basic_correctness() {
-    let (share_count, threshold) = (3, 1);
-    let KeySharesWithRecovery {
-        shares,
-        secret_recovery_keys,
-        session_nonce,
-    } = execute_keygen_with_recovery(share_count, threshold);
+    use rand::RngCore;
+
+    let (share_count, threshold) = (7, 4);
+    let mut secret_recovery_keys = vec![[0u8; 64]; share_count];
+
+    // repeat some secret_recovery_keys:
+    // fill secret_recovery_keys with random data, copying each entry once
+    // eg: [r1, r1, r2, r2, r3, r3, r4]
+    // use `while let` instead of `for` so as to enable use of iterator inside loop: https://stackoverflow.com/a/59045627
+    let mut iter_mut = secret_recovery_keys.iter_mut();
+    while let Some(s) = iter_mut.next() {
+        rand::thread_rng().fill_bytes(s);
+        if let Some(t) = iter_mut.next() {
+            *t = *s;
+        }
+    }
+    let session_nonce = b"foobar";
+    let shares = execute_keygen_from_recovery(threshold, &secret_recovery_keys, session_nonce);
+
     let recovery_infos = {
         let mut recovery_infos: Vec<_> = shares.iter().map(|s| s.recovery_info()).collect();
         recovery_infos.shuffle(&mut rand::thread_rng()); // simulate nondeterministic message receipt
@@ -19,7 +32,7 @@ fn basic_correctness() {
         .iter()
         .enumerate()
         .map(|(i, r)| {
-            SecretKeyShare::recover(r, &session_nonce, &recovery_infos, i, threshold).unwrap()
+            SecretKeyShare::recover(r, session_nonce, &recovery_infos, i, threshold).unwrap()
         })
         .collect();
 
