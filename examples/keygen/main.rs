afa7231c51b9e41fdbde2ccce8eb9ec67bc3aa04
Gus Gutoski <gus@axelar.network>
2021-06-23 15:30:00 -0400

    first draft keygen example (messy)

---
+++ examples/keygen/main.rs
@@ -0,0 +1,116 @@
+use rand::RngCore;
+use tofn::{
+    fillvec::FillVec,
+    protocol::gg20::SecretKeyShare,
+    refactor::{
+        keygen::{new_keygen, KeygenOutput},
+        protocol::protocol::{
+            RoundOutput::{self, *},
+            RoundWaiter,
+        },
+    },
+};
+
+/// TODO clean up
+
+fn main() {
+    let (share_count, threshold) = (5, 2);
+    let session_nonce = b"foobar";
+
+    let mut parties: Vec<RoundOutput<KeygenOutput>> = (0..share_count)
+        .map(|index| {
+            let mut secret_recovery_key = [0; 64];
+            rand::thread_rng().fill_bytes(&mut secret_recovery_key);
+            NotDone(
+                new_keygen(
+                    share_count,
+                    threshold,
+                    index,
+                    &secret_recovery_key,
+                    session_nonce,
+                )
+                .expect("`new_keygen` failure"),
+            )
+        })
+        .collect();
+
+    while nobody_done(&parties) {
+        parties = next_round(parties);
+    }
+
+    let results: Vec<SecretKeyShare> = parties
+        .into_iter()
+        .enumerate()
+        .map(|(i, party)| match party {
+            NotDone(_) => panic!("party {} not done yet", i),
+            Done(result) => result.expect("party finished with error"),
+        })
+        .collect();
+
+    println!("group info: {:?}", results[0].group);
+    for (i, result) in results.iter().enumerate() {
+        println!("party {} secret info: {:?}", i, result.share);
+    }
+}
+
+// TODO generic over final output F
+fn nobody_done(parties: &[RoundOutput<KeygenOutput>]) -> bool {
+    parties.iter().all(|party| matches!(party, NotDone(_)))
+}
+
+// TODO generic over final output F
+fn next_round(parties: Vec<RoundOutput<KeygenOutput>>) -> Vec<RoundOutput<KeygenOutput>> {
+    // extract current round from parties
+    let mut rounds: Vec<RoundWaiter<KeygenOutput>> = parties
+        .into_iter()
+        .enumerate()
+        .map(|(i, party)| match party {
+            NotDone(round) => round,
+            Done(_) => panic!("party {} done too early", i),
+        })
+        .collect();
+
+    // deliver bcasts
+    let bcasts: Vec<(usize, Vec<u8>)> = rounds
+        .iter()
+        .enumerate()
+        .filter_map(|(i, round)| round.bcast_out().map(|bcast| (i, bcast.clone())))
+        .collect();
+    for (from, bcast) in bcasts.into_iter() {
+        for round in rounds.iter_mut() {
+            round.bcast_in(from, &bcast);
+        }
+    }
+
+    // deliver p2ps
+    let all_p2ps: Vec<FillVec<Vec<u8>>> = rounds
+        .iter()
+        .map(|round| round.p2ps_out().clone())
+        .collect();
+    for (from, p2ps) in all_p2ps.into_iter().enumerate() {
+        for (to, p2p) in p2ps
+            .into_vec()
+            .into_iter()
+            .enumerate()
+            .filter_map(|(i, p2p)| p2p.map(|p| (i, p)))
+        {
+            for round in rounds.iter_mut() {
+                round.p2p_in(from, to, &p2p);
+            }
+        }
+    }
+
+    // compute next round's parties
+    rounds
+        .into_iter()
+        .enumerate()
+        .map(|(i, round)| {
+            assert!(
+                !round.expecting_more_msgs_this_round(),
+                "party {} should not be expecting more messages this round",
+                i
+            );
+            round.execute_next_round()
+        })
+        .collect()
+}
--- src/refactor/protocol/tests.rs
+++ src/refactor/protocol/tests.rs
@@ -1,11 +1,13 @@
 use super::protocol::*;
 
-pub fn execute_protocol_vec<F>(_parties: &[RoundWaiter<F>]) -> Vec<F> {
+pub fn execute_protocol_vec<F>(parties: &[RoundWaiter<F>]) -> Vec<F> {
     // while nobody_done(parties) {
-    //     for party  in parties {
-    //         party.
-    //     }
-
+    //     let parties: Vec<RoundWaiter<F>> = parties.iter().map(|p| p.execute_next_round()).collect();
     // }
+    // TODO if somebody's not done then we can't get a `F` from him!  Maybe just return `F` instead of `Vec<F>`??
     todo!()
+}
+
+fn nobody_done<F>(parties: &[RoundWaiter<F>]) -> bool {
+    parties.iter().all(|p| p.expecting_more_msgs_this_round())
 }
