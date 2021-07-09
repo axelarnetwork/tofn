f87590f8e526ad1b6af72e899c994410abc87b56
Gus Gutoski <gus@axelar.network>
2021-07-09 13:58:12 -0400

    lifetimes in Protocol (sad)

--- src/refactor/protocol_round/bcast_and_p2p.rs
+++ src/refactor/protocol_round/bcast_and_p2p.rs
@@ -1,49 +1,8 @@
-//! TODO traits only here, rename to `api` or `traits` or something.
-use self::executer::{ProtocolBuilder, RoundExecuterRaw};
-use crate::vecmap::{Behave, FillP2ps, FillVecMap, HoleVecMap, Index};
-use serde::{Deserialize, Serialize};
-use tracing::warn;
-
-use super::BytesVec;
-
-pub enum Protocol<'a, F, K>
+pub struct ProtocolRound<F, K>
 where
     K: Behave,
 {
-    NotDone(Box<dyn ProtocolRoundTrait<'a, FinalOutput = F, Index = K> + 'a>),
-    Done(ProtocolOutput<F, K>),
-}
-
-pub type ProtocolOutput<F, K> = Result<F, FillVecMap<K, Fault>>;
-
-// TODO rename to ProtocolRound, add : Send + Sync
-pub trait ProtocolRoundTrait<'a> {
-    type FinalOutput;
-    type Index: Behave;
-
-    fn bcast_out(&self) -> &Option<BytesVec>;
-    fn p2ps_out(&self) -> &Option<HoleVecMap<Self::Index, BytesVec>>;
-    fn bcast_in(&mut self, from: Index<Self::Index>, bytes: &[u8]);
-    fn p2p_in(&mut self, from: Index<Self::Index>, to: Index<Self::Index>, bytes: &[u8]);
-    fn expecting_more_msgs_this_round(&self) -> bool;
-    fn execute_next_round(self) -> Protocol<'a, Self::FinalOutput, Self::Index>;
-    fn party_count(&self) -> usize;
-    fn index(&self) -> Index<Self::Index>;
-
-    // TODO replace with round_as_any
-    #[cfg(test)]
-    fn round(
-        &self,
-    ) -> &Box<dyn RoundExecuterRaw<FinalOutput = Self::FinalOutput, Index = Self::Index>>;
-}
-
-// TODO rename to something with bcast_and_p2p
-pub struct ProtocolRound<'a, F, K>
-where
-    K: Behave + 'a,
-    F: 'a,
-{
-    round: Box<dyn RoundExecuterRaw<FinalOutput = F, Index = K> + 'a>,
+    round: Box<dyn RoundExecuterRaw<FinalOutput = F, Index = K>>,
     party_count: usize,
     index: Index<K>,
     bcast_out: Option<BytesVec>,
@@ -52,13 +11,12 @@ where
     p2ps_in: Option<FillP2ps<K, BytesVec>>,
 }
 
-impl<'a, F, K> ProtocolRound<'a, F, K>
+impl<F, K> ProtocolRound<F, K>
 where
-    K: Behave + 'a,
-    F: 'a,
+    K: Behave,
 {
     pub fn new(
-        round: Box<dyn RoundExecuterRaw<FinalOutput = F, Index = K> + 'a>,
+        round: Box<dyn RoundExecuterRaw<FinalOutput = F, Index = K>>,
         party_count: usize,
         index: Index<K>,
         bcast_out: Option<BytesVec>,
@@ -87,22 +45,13 @@ where
             p2ps_in,
         }
     }
-}
-impl<'a, F, K> ProtocolRoundTrait<'a> for ProtocolRound<'a, F, K>
-where
-    K: Behave + 'a,
-    F: 'a,
-{
-    type FinalOutput = F;
-    type Index = K;
-
-    fn bcast_out(&self) -> &Option<BytesVec> {
+    pub fn bcast_out(&self) -> &Option<BytesVec> {
         &self.bcast_out
     }
-    fn p2ps_out(&self) -> &Option<HoleVecMap<K, BytesVec>> {
+    pub fn p2ps_out(&self) -> &Option<HoleVecMap<K, BytesVec>> {
         &self.p2ps_out
     }
-    fn bcast_in(&mut self, from: Index<K>, bytes: &[u8]) {
+    pub fn bcast_in(&mut self, from: Index<K>, bytes: &[u8]) {
         if let Some(ref mut bcasts_in) = self.bcasts_in {
             // TODO range check
             bcasts_in.set_warn(from, bytes.to_vec());
@@ -110,7 +59,7 @@ where
             warn!("`bcast_in` called but no bcasts expected; discarding `bytes`");
         }
     }
-    fn p2p_in(&mut self, from: Index<K>, to: Index<K>, bytes: &[u8]) {
+    pub fn p2p_in(&mut self, from: Index<K>, to: Index<K>, bytes: &[u8]) {
         if let Some(ref mut p2ps_in) = self.p2ps_in {
             // TODO range checks
             p2ps_in.set_warn(from, to, bytes.to_vec());
@@ -118,7 +67,7 @@ where
             warn!("`p2p_in` called but no p2ps expected; discaring `bytes`");
         }
     }
-    fn expecting_more_msgs_this_round(&self) -> bool {
+    pub fn expecting_more_msgs_this_round(&self) -> bool {
         let expecting_more_bcasts = match self.bcasts_in {
             Some(ref bcasts_in) => !bcasts_in.is_full(),
             None => false,
@@ -132,41 +81,32 @@ where
         };
         expecting_more_p2ps
     }
-    fn execute_next_round(self) -> Protocol<'a, Self::FinalOutput, Self::Index> {
+    pub fn execute_next_round(self) -> Protocol<F, K> {
         match self.round.execute_raw(
             self.party_count,
             self.index,
             self.bcasts_in.unwrap_or_else(|| FillVecMap::with_size(0)), // TODO accept Option instead
             self.p2ps_in.unwrap_or_else(|| FillP2ps::with_size(0)), // TODO accept Option instead
         ) {
-            ProtocolBuilder::NotDone(builder) => Protocol::NotDone(Box::new(ProtocolRound::new(
+            ProtocolBuilder::NotDone(builder) => Protocol::NotDone(ProtocolRound::new(
                 builder.round,
                 self.party_count,
                 self.index,
                 builder.bcast_out,
                 builder.p2ps_out,
-            ))),
+            )),
             ProtocolBuilder::Done(output) => Protocol::Done(output),
         }
     }
-    fn party_count(&self) -> usize {
+    pub fn party_count(&self) -> usize {
         self.party_count
     }
-    fn index(&self) -> Index<K> {
+    pub fn index(&self) -> Index<K> {
         self.index
     }
 
     #[cfg(test)]
-    fn round(&self) -> &Box<dyn RoundExecuterRaw<FinalOutput = F, Index = K>> {
+    pub fn round(&self) -> &Box<dyn RoundExecuterRaw<FinalOutput = F, Index = K>> {
         &self.round
     }
-}
-
-pub mod executer;
-
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
-pub enum Fault {
-    MissingMessage,
-    CorruptedMessage,
-    ProtocolFault,
 }
