c7bd92eeca96519af143fe2fdf677ac08c4bb110
Gus Gutoski <gus@axelar.network>
2021-07-20 03:01:30 -0400

    add test for share recovery

--- src/refactor/keygen/tests.rs
+++ src/refactor/keygen/tests.rs
@@ -1,10 +1,10 @@
 use super::*;
 use crate::{
     protocol::gg20::vss_k256,
-    refactor::collections::{HoleVecMap, TypedUsize, VecMap},
+    refactor::collections::{zip2, HoleVecMap, TypedUsize, VecMap},
     refactor::sdk::api::{BytesVec, PartyShareCounts, Protocol},
 };
-use rand::RngCore;
+use rand::{prelude::SliceRandom, RngCore};
 use tracing_test::traced_test;
 
 #[cfg(feature = "malicious")]
@@ -28,20 +28,17 @@ fn basic_correctness() {
     }
 }
 
-pub(crate) fn execute_keygen(share_count: usize, threshold: usize) -> Vec<SecretKeyShare> {
+fn execute_keygen(share_count: usize, threshold: usize) -> Vec<SecretKeyShare> {
     execute_keygen_with_recovery(share_count, threshold).shares
 }
 
-pub(crate) struct KeySharesWithRecovery {
+struct KeySharesWithRecovery {
     pub shares: Vec<SecretKeyShare>,
     pub secret_recovery_keys: Vec<SecretRecoveryKey>,
     pub session_nonce: Vec<u8>,
 }
 
-pub(crate) fn execute_keygen_with_recovery(
-    share_count: usize,
-    threshold: usize,
-) -> KeySharesWithRecovery {
+fn execute_keygen_with_recovery(share_count: usize, threshold: usize) -> KeySharesWithRecovery {
     let mut secret_recovery_keys = vec![[0u8; 64]; share_count];
     for s in secret_recovery_keys.iter_mut() {
         rand::thread_rng().fill_bytes(s);
@@ -55,7 +52,7 @@ pub(crate) fn execute_keygen_with_recovery(
     }
 }
 
-pub(crate) fn execute_keygen_from_recovery(
+fn execute_keygen_from_recovery(
     threshold: usize,
     secret_recovery_keys: &[SecretRecoveryKey],
     session_nonce: &[u8],
@@ -262,5 +259,64 @@ fn t(n: usize, t: usize) -> TestCase {
     TestCase {
         share_count: n,
         threshold: t,
+    }
+}
+
+#[test]
+fn share_recovery() {
+    use rand::RngCore;
+
+    let (share_count, threshold) = (7, 4);
+    let mut secret_recovery_keys = vec![[0u8; 64]; share_count];
+
+    // repeat some secret_recovery_keys:
+    // fill secret_recovery_keys with random data, copying each entry once
+    // eg: [r1, r1, r2, r2, r3, r3, r4]
+    // use `while let` instead of `for` so as to enable use of iterator inside loop: https://stackoverflow.com/a/59045627
+    let mut iter_mut = secret_recovery_keys.iter_mut();
+    while let Some(s) = iter_mut.next() {
+        rand::thread_rng().fill_bytes(s);
+        if let Some(t) = iter_mut.next() {
+            *t = *s;
+        }
+    }
+    let session_nonce = b"foobar";
+    let shares = execute_keygen_from_recovery(threshold, &secret_recovery_keys, session_nonce);
+
+    let recovery_infos = {
+        let mut recovery_infos: Vec<_> =
+            shares.iter().map(|s| s.recovery_info().unwrap()).collect();
+        recovery_infos.shuffle(&mut rand::thread_rng()); // simulate nondeterministic message receipt
+        recovery_infos
+    };
+    let recovered_shares: Vec<SecretKeyShare> = secret_recovery_keys
+        .iter()
+        .enumerate()
+        .map(|(i, r)| {
+            SecretKeyShare::recover(
+                r,
+                session_nonce,
+                &recovery_infos,
+                TypedUsize::from_usize(i),
+                threshold,
+            )
+            .unwrap()
+        })
+        .collect();
+
+    assert_eq!(
+        recovered_shares, shares,
+        "comment-out this assert and use the following code to narrow down the discrepancy"
+    );
+
+    for (i, (s, r)) in shares.iter().zip(recovered_shares.iter()).enumerate() {
+        assert_eq!(s.share, r.share, "party {}", i);
+        for (j, ss, rr) in zip2(&s.group.all_shares, &r.group.all_shares) {
+            assert_eq!(ss.X_i, rr.X_i, "party {} public info on party {}", i, j);
+            assert_eq!(ss.ek, rr.ek, "party {} public info on party {}", i, j);
+            assert_eq!(ss.zkp, rr.zkp, "party {} public info on party {}", i, j);
+        }
+        assert_eq!(s.group.threshold, r.group.threshold, "party {}", i);
+        assert_eq!(s.group.y, r.group.y, "party {}", i);
     }
 }
