281225eea11ad3ebd8dd63b66c85b8b2ea3cca99
Milap Sheth <milap@axelar.network>
2021-08-04 01:26:29 -0400

    switch up test

--- tests/integration/multi_thread/party.rs
+++ tests/integration/multi_thread/party.rs
@@ -26,24 +26,18 @@ where
 {
     let mut r = 0;
 
+    // We keep track of when all parties are done receiving protocol messages
+    // This way all parties move into the next round together to avoid
+    // concurrency issues mentioned in https://github.com/axelarnetwork/tofn/issues/102
+    // NOTE: This is only a workaround for tests where ordering is not guaranteed
+    // unlike when using a blockchain.
+    let mut done_parties: usize = 0;
+
     while let Protocol::NotDone(mut round) = party {
         let id = round.info().share_info().share_id();
 
         let party_id = round.info().party_id();
         let total_shares = round.info().party_share_counts().total_share_count();
-
-        // We keep track of when all parties are done receiving protocol messages
-        // This way all parties move into the next round together to avoid
-        // concurrency issues mentioned in https://github.com/axelarnetwork/tofn/issues/102
-        // NOTE: This is only a workaround for tests where ordering is not guaranteed
-        // unlike when using a blockchain.
-        let mut done_parties: usize = 0;
-
-        // Send a signal to all parties that we've received all messages
-        broadcaster.send(Message {
-            from: party_id,
-            bytes: Vec::new(),
-        });
 
         while done_parties < total_shares {
             let msg = input.recv().expect("recv fail");
@@ -54,6 +48,8 @@ where
                 round.msg_in(msg.from, &msg.bytes)?;
             }
         }
+
+        done_parties = 0;
 
         info!("Peer {}, Round {}: sending out messages", id, r);
         // send outgoing messages
@@ -85,6 +81,12 @@ where
         info!("Peer {}, Round {}: starting", id, r);
 
         party = round.execute_next_round()?;
+
+        // Send a signal to all parties that we've received all messages
+        broadcaster.send(Message {
+            from: party_id,
+            bytes: Vec::new(),
+        });
     }
     match party {
         Protocol::NotDone(_) => unreachable!(),
