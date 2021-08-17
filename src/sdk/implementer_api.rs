daefda0206618946ebc7882bb99bd386e89b267c
Gus Gutoski <gus@axelar.network>
2021-08-17 19:59:35 -0400

    replace implementer_api/ with implementer_api.rs

---
+++ src/sdk/implementer_api.rs
@@ -0,0 +1,39 @@
+//! API for protocol implementers, but not for users of protocols
+pub use super::executer::{Executer, ExecuterRaw};
+pub use super::protocol::xnew_protocol;
+pub use super::protocol_builder::{ProtocolBuilderOutput, XProtocolBuilder, XRoundBuilder};
+pub use super::protocol_info::ProtocolInfo;
+
+mod utils {
+    use crate::{
+        collections::TypedUsize,
+        sdk::api::{BytesVec, TofnFatal, TofnResult},
+    };
+    use tracing::{error, info, warn};
+
+    pub fn serialize<T: ?Sized>(value: &T) -> TofnResult<BytesVec>
+    where
+        T: serde::Serialize,
+    {
+        match bincode::serialize(value) {
+            Ok(bytes) => Ok(bytes),
+            Err(err) => {
+                error!("serialization failure: {}", err.to_string());
+                Err(TofnFatal)
+            }
+        }
+    }
+
+    pub fn log_fault_info<K>(me: TypedUsize<K>, faulter: TypedUsize<K>, fault: &str) {
+        info!("peer {} detected [{}] by {}", me, fault, faulter);
+    }
+
+    pub fn log_fault_warn<K>(me: TypedUsize<K>, faulter: TypedUsize<K>, fault: &str) {
+        warn!("peer {} detected [{}] by {}", me, fault, faulter);
+    }
+
+    pub fn log_accuse_warn<K>(me: TypedUsize<K>, faulter: TypedUsize<K>, fault: &str) {
+        warn!("peer {} accused {} of [{}]", me, faulter, fault);
+    }
+}
+pub use utils::{log_accuse_warn, log_fault_info, log_fault_warn, serialize};
--- src/sdk/implementer_api/mod.rs
+++
@@ -1,8 +0,0 @@
-//! API for protocol implementers, but not for users of protocols
-pub use super::executer::{Executer, ExecuterRaw};
-pub use super::protocol::xnew_protocol;
-pub use super::protocol_builder::{ProtocolBuilderOutput, XProtocolBuilder, XRoundBuilder};
-pub use super::protocol_info::ProtocolInfo;
-
-mod utils;
-pub use utils::{log_accuse_warn, log_fault_info, log_fault_warn, serialize};
