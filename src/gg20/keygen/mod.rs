763a7efa198e053c0ec92311aa51a532784910c1
Gus Gutoski <gus@axelar.network>
2021-09-23 17:05:35 -0400

    move secret_key_share into api.rs

--- src/gg20/keygen/mod.rs
+++ src/gg20/keygen/mod.rs
@@ -1,13 +1,12 @@
 mod api;
 pub use api::*;
-mod secret_key_share;
-pub use secret_key_share::*;
 
 mod r1;
 mod r2;
 mod r3;
 mod r4;
 mod rng;
+mod secret_key_share;
 
 #[cfg(test)]
 pub(super) mod tests; // pub(super) so that sign module can see tests::execute_keygen
