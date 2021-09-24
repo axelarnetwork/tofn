bd6c90cf41a42dcbe457e6a8b9827ab50da3825e
Milap Sheth <milap@axelar.network>
2021-09-25 02:21:14 +0400

    Updated benchmarking

--- benches/safe_primes.rs
+++ benches/safe_primes.rs
@@ -1,15 +1,29 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use libpaillier::{DecryptionKey, EncryptionKey};
+use libpaillier::{unknown_order::BigNumber, DecryptionKey, EncryptionKey};
 use rand::{CryptoRng, RngCore, SeedableRng};
 use rand_chacha::ChaCha20Rng;
 
-pub fn unsafe_primes(c: &mut Criterion) {
+pub fn safe_primes(c: &mut Criterion) {
     let mut rng = chacha_rng();
-    // TODO: Expose unsafe prime keygen for benchmarking
-    // c.bench_function("unsafe primes", |b| b.iter(|| DecryptionKey::with_rng(&mut rng)));
 
-    let mut g = c.benchmark_group("safe-primes-group");
+    let mut g = c.benchmark_group("paillier-keypair-gen");
     g.sample_size(10);
+
+    g.bench_function("unsafe primes", |b| {
+        b.iter(|| {
+            let p = BigNumber::prime_with_rng(&mut rng, 1024);
+            let q = BigNumber::prime_with_rng(&mut rng, 1024);
+
+            let dk = DecryptionKey::with_safe_primes_unchecked(&p, &q).unwrap();
+
+            let ek: EncryptionKey = (&dk).into();
+
+            (ek, dk)
+        })
+    });
+
+    let mut rng = chacha_rng();
+
     g.bench_function("safe primes", |b| {
         b.iter(|| {
             let dk = DecryptionKey::with_rng(&mut rng).unwrap();
@@ -21,7 +35,7 @@ pub fn unsafe_primes(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, unsafe_primes);
+criterion_group!(benches, safe_primes);
 criterion_main!(benches);
 
 // initialize a deterministic rng to conserve random bits
