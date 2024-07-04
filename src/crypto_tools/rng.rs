dd98fcdd9d15c0d56e4a00d653029fefb1e44723
Milap Sheth <milap@interoplabs.io>
2024-07-04 18:13:35 -0400

    test: add rng known vector tests

--- src/crypto_tools/rng.rs
+++ src/crypto_tools/rng.rs
@@ -162,3 +162,109 @@ pub fn dummy_secret_recovery_key(index: usize) -> SecretRecoveryKey {
     }
     SecretRecoveryKey(result)
 }
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn rng_seed_signing_key_known_vectors() {
+        struct TestCase {
+            protocol_tag: u8,
+            tag: u8,
+            secret_recovery_key: SecretRecoveryKey,
+            session_nonce: Vec<u8>,
+            expected_output: [u8; 32],
+        }
+
+        let test_cases = vec![
+            TestCase {
+                protocol_tag: 0,
+                tag: 0,
+                secret_recovery_key: SecretRecoveryKey([0; 64]),
+                session_nonce: vec![0; 4],
+                expected_output: [
+                    178, 195, 90, 168, 218, 224, 244, 241, 22, 212, 134, 206, 49, 137, 57, 138,
+                    175, 204, 132, 22, 121, 4, 175, 173, 27, 119, 145, 174, 104, 1, 204, 121,
+                ],
+            },
+            TestCase {
+                protocol_tag: 0x01,
+                tag: 0x02,
+                secret_recovery_key: SecretRecoveryKey([0x11; 64]),
+                session_nonce: vec![0xAA, 0xBB, 0xCC, 0xDD],
+                expected_output: [
+                    195, 251, 156, 25, 253, 219, 100, 153, 17, 181, 174, 140, 167, 35, 103, 217,
+                    158, 164, 189, 241, 128, 100, 57, 12, 109, 144, 130, 212, 6, 255, 90, 56,
+                ],
+            },
+        ];
+
+        for test_case in test_cases {
+            let mut rng = rng_seed_signing_key(
+                test_case.protocol_tag,
+                test_case.tag,
+                &test_case.secret_recovery_key,
+                &test_case.session_nonce,
+            )
+            .expect("Failed to initialize RNG");
+
+            let mut output = [0u8; 32];
+            rng.fill_bytes(&mut output);
+
+            assert_eq!(output, test_case.expected_output);
+        }
+    }
+
+
+    #[test]
+    fn rng_seed_ecdsa_ephemeral_scalar_known_vectors() {
+        struct TestCase {
+            protocol_tag: u8,
+            tag: u8,
+            signing_key: k256::Scalar,
+            message_digest: k256::Scalar,
+            expected_output: [u8; 32],
+        }
+
+        let test_cases = vec![
+            TestCase {
+                protocol_tag: 0,
+                tag: 0,
+                signing_key: k256::Scalar::from_bytes_reduced(&[0; 32].into()),
+                message_digest: k256::Scalar::from_bytes_reduced(&[0; 32].into()),
+                expected_output: [42, 244, 242, 58, 19, 84, 31, 121, 130, 37, 135, 111, 107, 172, 251, 213, 74, 30, 235, 53, 166, 76, 21, 34, 197, 232, 120, 78, 112, 230, 226, 123],
+            },
+            TestCase {
+                protocol_tag: 0,
+                tag: 0,
+                signing_key: k256::Scalar::from_bytes_reduced(&[1; 32].into()),
+                message_digest: k256::Scalar::from_bytes_reduced(&[2; 32].into()),
+                expected_output: [114, 196, 139, 208, 156, 99, 210, 57, 81, 207, 137, 119, 190, 163, 38, 157, 231, 107, 201, 6, 70, 109, 193, 33, 80, 74, 17, 33, 220, 160, 216, 40],
+            },
+            TestCase {
+                protocol_tag: 1,
+                tag: 2,
+                signing_key: k256::Scalar::from_bytes_reduced(&[255; 32].into()),
+                message_digest: k256::Scalar::from_bytes_reduced(&[255; 32].into()),
+                expected_output: [170, 189, 238, 222, 51, 5, 123, 112, 253, 104, 67, 99, 69, 239, 234, 10, 112, 225, 213, 0, 159, 175, 186, 10, 213, 159, 40, 39, 233, 46, 40, 156],
+            },
+        ];
+
+        for test_case in test_cases {
+            let mut rng = rng_seed_ecdsa_ephemeral_scalar(
+                test_case.protocol_tag,
+                test_case.tag,
+                &test_case.signing_key,
+                &test_case.message_digest,
+            )
+            .expect("Failed to initialize RNG");
+
+            let mut output = [0u8; 32];
+            rng.fill_bytes(&mut output);
+
+            assert_eq!(output, test_case.expected_output);
+        }
+    }
+}
