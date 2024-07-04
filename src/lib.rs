1cc9a806654d595166c7719fbddc92ede459777c
Milap Sheth <milap@interoplabs.io>
2024-07-04 18:13:49 -0400

    chore: disable ed25519

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,8 @@ pub mod collections;
 mod constants;
 mod crypto_tools;
 pub mod ecdsa;
-pub mod ed25519;
+// ed25519 isn't used on this branch
+// pub mod ed25519;
 pub mod gg20;
 pub mod multisig;
 pub mod sdk;
