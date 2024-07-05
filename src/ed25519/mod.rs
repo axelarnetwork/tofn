fec9f8d8d0c09fa051f5c88808ef41596822244f
Milap Sheth <milap@interoplabs.io>
2024-07-05 15:12:58 -0400

    add ed25519 known vector tests

--- src/ed25519/mod.rs
+++ src/ed25519/mod.rs
@@ -95,7 +95,7 @@ pub struct Asn1Signature<'a> {
 #[cfg(test)]
 mod tests {
     use super::{keygen, sign, verify};
-    use crate::sdk::key::dummy_secret_recovery_key;
+    use crate::sdk::key::{dummy_secret_recovery_key, SecretRecoveryKey};
 
     #[test]
     fn keygen_sign_decode_verify() {
@@ -125,5 +125,62 @@ mod tests {
         .unwrap();
 
         assert!(!success);
+    }
+
+    /// Check keygen/signing outputs against known vectors to catch regressions (such as on updating deps).
+    /// Known vectors were generated from tofn v0.2.0 release when ed25519 was added.
+    #[test]
+    fn keygen_sign_known_vectors() {
+        struct TestCase {
+            secret_recovery_key: SecretRecoveryKey,
+            session_nonce: Vec<u8>,
+            message_digest: [u8; 32],
+            expected_signing_key: Vec<u8>,
+            expected_verifying_key: Vec<u8>,
+            expected_signature: Vec<u8>,
+        }
+
+        let test_cases = vec![
+            TestCase {
+                secret_recovery_key: SecretRecoveryKey([0; 64]),
+                session_nonce: vec![0; 4],
+                message_digest: [42; 32],
+                expected_signing_key: vec![47, 167, 96, 110, 72, 189, 202, 24, 85, 172, 92, 89, 34, 214, 183, 208, 123, 207, 108, 6, 203, 152, 106, 30, 152, 22, 213, 246, 196, 228, 15, 203],
+                expected_verifying_key: vec![252, 19, 166, 220, 100, 224, 119, 124, 12, 62, 248, 29, 85, 210, 67, 70, 106, 30, 134, 23, 122, 102, 202, 128, 167, 87, 162, 1, 162, 174, 45, 87],
+                expected_signature: vec![48, 74, 48, 5, 6, 3, 43, 101, 112, 3, 65, 0, 251, 109, 114, 195, 237, 52, 143, 102, 5, 58, 186, 163, 235, 93, 247, 31, 92, 253, 185, 204, 85, 228, 29, 255, 109, 62, 188, 254, 212, 70, 27, 227, 40, 206, 112, 248, 107, 34, 173, 148, 68, 243, 180, 4, 43, 81, 199, 202, 8, 204, 227, 191, 144, 66, 127, 160, 148, 159, 242, 190, 39, 118, 166, 2],
+            },
+            TestCase {
+                secret_recovery_key: SecretRecoveryKey([0xff; 64]),
+                session_nonce: vec![0xff; 32],
+                message_digest: [0xff; 32],
+                expected_signing_key: vec![55, 236, 122, 218, 96, 136, 145, 36, 23, 44, 197, 200, 91, 19, 123, 136, 19, 0, 16, 129, 189, 183, 22, 75, 120, 46, 123, 71, 132, 27, 161, 232],
+                expected_verifying_key: vec![35, 64, 196, 184, 99, 43, 153, 241, 236, 145, 132, 185, 168, 32, 145, 236, 146, 250, 111, 22, 9, 14, 95, 163, 52, 100, 192, 16, 201, 62, 98, 58],
+                expected_signature: vec![48, 74, 48, 5, 6, 3, 43, 101, 112, 3, 65, 0, 40, 140, 38, 241, 254, 10, 159, 193, 167, 209, 223, 22, 32, 55, 44, 214, 141, 85, 199, 79, 191, 72, 254, 170, 175, 226, 71, 40, 88, 75, 238, 129, 211, 153, 211, 50, 207, 216, 137, 146, 146, 17, 92, 42, 55, 122, 27, 221, 198, 236, 229, 77, 78, 222, 210, 85, 177, 80, 6, 139, 169, 223, 154, 10],
+            },
+        ];
+
+        for test_case in test_cases {
+            let keypair = keygen(&test_case.secret_recovery_key, &test_case.session_nonce).unwrap();
+
+            assert_eq!(
+                &(keypair.0.to_bytes())[..],
+                &test_case.expected_signing_key
+            );
+            assert_eq!(
+                keypair.encoded_verifying_key().to_vec(),
+                test_case.expected_verifying_key
+            );
+
+            let signature = sign(&keypair, &test_case.message_digest.into()).unwrap();
+            assert_eq!(signature, test_case.expected_signature);
+
+            let success = verify(
+                &keypair.encoded_verifying_key(),
+                &test_case.message_digest.into(),
+                &signature,
+            )
+            .unwrap();
+            assert!(success);
+        }
     }
 }
